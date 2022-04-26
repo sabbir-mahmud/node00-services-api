4539d40b491c11f349a789b018d21b55ea171ef4
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2022-04-26 16:19:44 +0600

    server ok

--- index.js
+++ index.js
@@ -39,27 +39,54 @@ async function servicesApi() {
     try {
         await client.connect();
         console.log('Connected to MongoDB');
-        const servicesCollection = client.db('Services').collection('service');
+        const productsCollection = client.db('E-shop').collection('products');
         /**
          * --------------------------------------------------
          * Get All Services
          * --------------------------------------------------
          */
-        app.get('/api/services', async (req, res) => {
-            const services = await servicesCollection.find().toArray();
+        app.get('/api/products', async (req, res) => {
+            const services = await productsCollection.find().toArray();
             res.send(services);
         });
+        /**
+         * --------------------------------------------------
+         * Get Single Service
+         * 
+         */
+        app.get('/api/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const service = await productsCollection.findOne({ _id: ObjectId(id) });
+            res.send(service);
+        })
 
         /**
          * --------------------------------------------------
          * add new service
          * --------------------------------------------------
          */
-        app.post('/api/services', async (req, res) => {
+        app.post('/api/products', async (req, res) => {
             const newService = req.body;
-            const result = await servicesCollection.insertOne(newService);
+            const result = await productsCollection.insertOne(newService);
             res.send(result);
         });
+
+        /**
+         * --------------------------------------------------
+         * product get by ordered details
+         * --------------------------------------------------
+         */
+
+        app.post('/api/product/order', async (req, res) => {
+            const ordered_details = req.body;
+            const ids = ordered_details.map(id => ObjectId(id));
+            console.log(ordered_details);
+            const query = { _id: { $in: ids } };
+            const products = await productsCollection.find(query).toArray();
+            res.send(products);
+        })
+
+
     } finally {
         /**
          * --------------------------------------------------
@@ -72,6 +99,84 @@ async function servicesApi() {
 
 }
 servicesApi().catch(console.dir)
+
+/**
+ * --------------------------------------------------
+ * order api
+ * --------------------------------------------------
+ */
+
+async function orderApi() {
+    try {
+        /**
+         * --------------------------------------------------
+         * Connect to MongoDB
+         * --------------------------------------------------
+         */
+        await client.connect();
+        console.log('Connected to order');
+        const ordersCollection = client.db('E-shop').collection('orders');
+
+        /**
+         * --------------------------------------------------
+         * get orders
+         * --------------------------------------------------
+         */
+        app.get('/api/orders', async (req, res) => {
+            const email = req.query.email;
+            if (email) {
+                const orders = await ordersCollection.find({ email: email }).toArray();
+                res.send(orders);
+            } else {
+                const orders = await ordersCollection.find().toArray();
+                res.send(orders);
+            }
+        });
+
+        /**
+         * --------------------------------------------------
+         * add new order
+         * --------------------------------------------------
+         */
+        app.post('/api/orders', async (req, res) => {
+            const p_id = req.body.p_id;
+            const email = req.body.email;
+            let qtn;
+            let product;
+            const cursor = await ordersCollection.find({ p_id: p_id, email: email }).toArray();
+            if (cursor[0]?.p_id === p_id && cursor[0]?.email === email) {
+                qtn = cursor[0].qtn + 1;
+                console.log(qtn);
+                const result = await ordersCollection.updateOne({ p_id: p_id, email: email }, { $set: { qtn: qtn } });
+                console.log(result);
+                res.send(result);
+
+            } else {
+                qtn = 1;
+                product = {
+                    p_id: p_id,
+                    qtn: qtn,
+                    email: email
+                }
+                const result = await ordersCollection.insertOne(product);
+                res.send(result);
+            }
+
+
+        });
+    }
+    finally {
+        /**
+        * --------------------------------------------------
+        * Close the connection
+        * --------------------------------------------------
+        */
+        // client.close();
+    }
+
+}
+
+orderApi().catch(console.dir)
 
 
 
